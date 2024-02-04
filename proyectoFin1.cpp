3a0aefed78679ca4dd146bc628bd4e0c06376436
David D. <daizpaz19@gmail.com>
2024-02-04 14:37:02 -0600

    PROYECTO UNIDAD 1: 1. contar la cantidad de palabras ----OK 2. contar oraciones ----OK 3. contar párrafos ----OK 4. calcular la longitud promedio de las palabras ----OK

--- proyectoFin1.cpp
+++ proyectoFin1.cpp
@@ -1,32 +1,58 @@
 #include <iostream>
 #include <string>
+#include <ctype.h>
 
 using namespace std;
 
+/* Cree un programa que lea texto de un archivo (10 pts) y lo analice, generando un archivo llamado
+Reporte.txt (10 pts).
+Deberá pedir al usuario el nombre del archivo .txt (10 pts)
+Implemente las funciones: (10 pts c/u)
+1. contar la cantidad de palabras ----OK
+2. contar oraciones ----OK
+3. contar párrafos ----OK
+4. calcular la longitud promedio de las palabras ----OK
+5. identificar las 3 palabras más comunes */
+
 int main(void){
 
-  string frase = "Hola mundo. Espero que estes bien.   ";
-  int letras = 0, palabras = 0, aux;
+  string frase = "Hola mundo. Espero que estes bien. Hola. ";
+  int letras = 0, palabras = 0, oraciones = 0, parrafos = 1;
+  double promPal = 0; 
+  bool aux; 
 
   for(int i = 0; i < frase.length(); i++){
-    if((frase[i] <= 90 && frase[i] >= 65) || (frase[i] >= 97 && frase[i] <= 122)){
+    // conteo de palabras
+    aux = false;
+    while(isalpha(frase[i])){
+      aux = true;
       letras++;
+      i++;
+    } 
+    if(aux == true){
+      palabras++;
+    }
+
+    // conteo de oraciones
+    if(frase[i] == '.'){
+      oraciones++;
+    }
+
+    // conteo de parrafos
+    if(frase[i] == '\n'){
+      parrafos++;
     }
   }
 
-  for(int i = 0; i < frase.length(); i++){
-    if(!((frase[i] <= 90 && frase[i] >= 65) || (frase[i] >= 97 && frase[i] <= 122))){
-      if(frase[i+2] != '\0'){
-        palabras++;
-      }
-    }
-  }
+  // promedio de longitud por palabra
+  promPal = (letras*1.0)/palabras; 
 
-  aux = letras%palabras;
-
-  cout << aux << "\n";
   cout << "Letras: " << letras << "\n";
   cout << "Palabras: " << palabras << "\n";
+  cout << "Oraciones: " << oraciones << "\n";
+  cout << "Parrafos: " << parrafos << "\n";
+  cout << "Longitud promedio de palabras: " << promPal << "\n";
+
   getchar();
   return 0;
 }
